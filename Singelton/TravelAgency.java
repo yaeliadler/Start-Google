74ea5da206cd12b88c7de90194f6710490abaf14
Yael Adler <yaeli.adler@gmail.com>
2022-11-01 11:45:37 +0200

    createVehicle function was created with switch

--- Singelton/TravelAgency.java
+++ Singelton/TravelAgency.java
@@ -7,5 +7,18 @@ public class TravelAgency {
         TAXI,
         BOAT
     }
-
+    public Agency createVehicle (VehicleType type){
+        switch (type){
+            case BUS:
+                return new RentBus();
+            case BOAT:
+                return new RentBoat();
+            case PLANE:
+                return new RentPlane();
+            case TAXI:
+                return new REntTaxi();
+            default:
+                throw new IllegalArgumentException(String.format("Vehicle type not supported %s", type));
+        }
+    }
 }
